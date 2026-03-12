48680066f925f1c379a784a795cd55bb48d4f74d
villa <evilasio.junior@ifba.edu.br>
2026-03-11 21:52:29 -0300

    Fase 1 Completa - Iniciar Fase 2

--- JogoDaVelha/src/main/java/br/edu/ifba/euc/jogodavelha/JogoDaVelhaController.java
+++ JogoDaVelha/src/main/java/br/edu/ifba/euc/jogodavelha/JogoDaVelhaController.java
@@ -1,5 +1,18 @@
 package br.edu.ifba.euc.jogodavelha;
 
 public class JogoDaVelhaController {
+    //Defina uma view e uma model privadas
     
+    //Defina um construtor que recebe uma view e uma model por parâmetro
+    //O construtor também deve controlar os eventos da view:
+    //      this.view.setActionListeners (this);
+
+
+    //Implemente/Sobrescreva o método obrigatório actionPerformed
+    /*
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    */
 }
--- JogoDaVelha/src/main/java/br/edu/ifba/euc/jogodavelha/JogoDaVelhaView.java
+++ JogoDaVelha/src/main/java/br/edu/ifba/euc/jogodavelha/JogoDaVelhaView.java
@@ -1,7 +1,9 @@
 package br.edu.ifba.euc.jogodavelha;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -23,6 +25,29 @@ public class JogoDaVelhaView extends JFrame{
         setSize(300, 350);
         setLayout(new BorderLayout());
         painelDeBotoes = new JPanel(new GridLayout(3,3));
-        
+        painelDeBotoes = new JPanel(new GridLayout(3,3));
+        for(int linha = 0;linha < 3; linha++){
+            for(int coluna = 0; coluna < 3; coluna++){
+                botoes[linha][coluna] = new JButton();
+                botoes[linha][coluna].setFont(new Font("Arial", Font.BOLD, 40));
+                painelDeBotoes.add(botoes[linha][coluna]);
+            }
+        }
+        add(painelDeBotoes, BorderLayout.CENTER);
+        add(mensagem, BorderLayout.SOUTH);
+    }
+    
+    //Componentes visual que terão eventos "escutados"
+    public void setActionListeners(ActionListener listener) {
+
+    }
+    
+    //Getters
+    public JButton[][] getBotoes() {
+        return botoes;
+    }
+
+    public JLabel getMensagem() {
+        return mensagem;
     }
 }
